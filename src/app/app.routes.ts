88842744af1a55831f9c1d7568302e64c18ea369
PianoNic <79938743+Pianonic@users.noreply.github.com>
2025-03-16 19:28:04 +0100

    Add 404 Not Found handling and update routing for DiagrammComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DiagrammComponent } from './diagramm/diagramm.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    {path:"diagramm", component: DiagrammComponent}
-];
+    { path: 'plan/:file', component: DiagrammComponent } ,
+    { path: '**', component: NotFoundComponent}
+  ];
--- src/app/diagramm/diagramm.component.ts
+++ src/app/diagramm/diagramm.component.ts
@@ -3,6 +3,7 @@ import { XmlParserService } from '../services/xml-parser.service';
 import { Systemebene } from '../models/Systemebene';
 import { HttpClient } from '@angular/common/http';
 import { NgClass, NgFor, NgIf } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-diagramm',
@@ -13,29 +14,41 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
 })
 export class DiagrammComponent implements OnInit {
   systemebene: Systemebene | null = null;
-  
-  constructor(private xmlParserService: XmlParserService, private httpClient: HttpClient) {}
-  
+
+  constructor(
+    private xmlParserService: XmlParserService,
+    private httpClient: HttpClient,
+    private route: ActivatedRoute,
+    private router: Router // Router hinzufügen
+  ) {}
+
   ngOnInit(): void {
-    this.loadXmlFile();
+    const fileName = this.route.snapshot.paramMap.get('file');
+    if (!fileName) {
+      this.router.navigate(['/404']); // Nutzung von Angular Router für die Weiterleitung
+      return;
+    }
+    this.loadXmlFile(fileName + ".xml");
   }
 
-  loadXmlFile(): void {
-    this.httpClient.get('/IntegrationSharePoint-basiertesIntranet.xml', { responseType: 'text' })
+  loadXmlFile(filename: string): void {
+    this.httpClient.get(filename, { responseType: 'text' })
       .subscribe({
         next: (xmlString) => {
           this.xmlParserService.parseXml(xmlString).subscribe({
             next: (data) => {
               this.systemebene = data;
-              console.log(data);
+              //console.log(data);
             },
             error: (err) => {
               console.error('Fehler beim Parsen:', err);
+              this.router.navigate(['/404']); // Weiterleitung bei Fehler
             }
           });
         },
         error: (err) => {
           console.error('Fehler beim Laden der XML-Datei:', err);
+          this.router.navigate(['/404']); // Weiterleitung bei Fehler
         }
       });
   }
